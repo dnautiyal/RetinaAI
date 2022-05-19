1905353b958948b002cfe64a3062c297259fcfe7
Deepak Nautiyal <deepaknautiyal@hotmail.com>
2022-05-19 15:05:32 -0400

    added 3 model support

--- retinaai/retinaai/prediction.py
+++ retinaai/retinaai/prediction.py
@@ -6,7 +6,9 @@ from .model import RetinaModel
 import logging
 # logging.basicConfig(filename='/opt/python/log/retinaai.log', level=logging.DEBUG)
 logger = logging.getLogger(__name__)
-deepak_model = RetinaModel('Retinal_OCT_INCEPTIONV3-04062022-00-33.tflite', 'INCEPTIONV3', 256)
+model_1 = RetinaModel('Retinal_OCT_INCEPTIONV3-04062022-00-33.tflite', 'INCEPTIONV3', 256)
+model_2 = RetinaModel('Retinal_OCT_RESNET50-05092022-20-57.tflite', 'RESNET50', 256)
+model_3 = RetinaModel('Retinal_OCT_VGG19-05172022-21-38.tflite', 'VGG19', 256)
 def get_category(img):
     """Write a Function to Predict the Class Name
 
@@ -17,8 +19,11 @@ def get_category(img):
         [str]: Prediction
     """
     logger.info("calling model.predict method")
-    pred = deepak_model.predict(img)
-    return pred
+    pred1 = model_1.predict(img)
+    pred2 = model_2.predict(img)
+    pred3 = model_3.predict(img)
+    ret_val = '{}: {}; {}: {}; {}: {}'.format(model_1.get_model_description(), pred1,model_2.get_model_description(),pred2,model_3.get_model_description(),pred3)
+    return ret_val
 
 
 def plot_category(img):
